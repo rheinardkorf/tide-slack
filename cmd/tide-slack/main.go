9d14e318f3077001f3ec9b3e08b13bfc329a17af
Rheinard Korf <rheinard@thekorfs.com>
2018-07-21 02:33:06 +1000

    experiment

--- cmd/tide-slack/main.go
+++ cmd/tide-slack/main.go
@@ -6,6 +6,12 @@ import (
 	"net/http"
 	"fmt"
 	"os"
+	"github.com/nlopes/slack"
+)
+
+var (
+	token = os.Getenv("TOKEN")
+	teamDomain = os.Getenv("TEAM")
 )
 
 func handleOauth(w http.ResponseWriter, r *http.Request) {
@@ -13,7 +19,21 @@ func handleOauth(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleTideCommand(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "/tide handler")
+
+	//s := slack.New(token)
+	s, err := slack.SlashCommandParse(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	//
+	//if !s.ValidateToken(token) {
+		//w.WriteHeader(http.StatusUnauthorized)
+		//fmt.Fprintf(w, "awwwww" )
+		//return
+	//}
+
+	fmt.Fprintf(w, "/tide handler: " + s.TeamDomain )
 }
 
 func main() {
